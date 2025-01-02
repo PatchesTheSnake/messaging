bca675659cae3146910054e804b50899dc2fc13c
PatchesTheSnake <94875046+PatchesTheSnake@users.noreply.github.com>
2025-01-02 16:25:43 -0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ messageing.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
     messageing.sendString("testr")
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.EightG, function () {
     //messageing.connect(34)
     messageing.sendString("test")
 
