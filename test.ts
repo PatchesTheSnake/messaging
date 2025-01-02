5c4d5279e875a4214db09c78541f02f8430e504e
PatchesTheSnake <94875046+PatchesTheSnake@users.noreply.github.com>
2025-01-02 16:29:06 -0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,10 +1,10 @@
-// Add your code here
-messageing.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
-    messageing.sendString("testr")
-})
-input.onGesture(Gesture.EightG, function () {
-    //messageing.connect(34)
-    messageing.sendString("test")
+// // Add your code here
+// messageing.onReceivedString(function (receivedString) {
+//     basic.showString(receivedString)
+//     messageing.sendString("testr")
+// })
+// input.onGesture(Gesture.EightG, function () {
+//     //messageing.connect(34)
+//     messageing.sendString("test")
 
-})
+// })
